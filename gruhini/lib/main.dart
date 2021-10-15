e36278dee7588aabcdf67aa264d29893e6f731bb
shivanisorte <sssorte1428@gmail.com>
2021-10-15 20:24:07 +0530

    added nav drawer

--- gruhini/lib/main.dart
+++ gruhini/lib/main.dart
@@ -65,11 +65,53 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Gruhini")),
-      body: Center(
-          child: Text(
-        "Dashboard",
-        textScaleFactor: 2,
-      )),
+      body: const Center(
+        child: Text('My Dashboard'),
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+              ),
+              child: Text('Hello, Friend!'),
+            ),
+            ListTile(
+              title: const Text('My Dashboard'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('My Profile'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('About App'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
