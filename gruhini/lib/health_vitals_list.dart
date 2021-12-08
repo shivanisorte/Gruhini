27c997851a234fe317ba63eaedf3ffa03b009c96
susmitpatil3136 <64474529+susmitpatil3136@users.noreply.github.com>
2021-12-08 14:45:20 +0530

    updated health vitals logo

---
+++ gruhini/lib/health_vitals_list.dart
@@ -0,0 +1,289 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'ComingSoonScreen.dart';
+
+class Health_vitals_list extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Color(0xfff8f9fa),
+
+      appBar: AppBar(
+        title: Text("HoMed"),
+        backgroundColor: Color(0xff42a5f5),
+      ),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 20,
+            ),
+
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("WELCOME TO HoMed APP",
+                          style: TextStyle(
+                              color: Color(0xff023e8a),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold)),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text("Health Vitals List ",
+                          style: TextStyle(
+                              color: Color(0xff023e8a),
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                  IconButton(
+                    alignment: Alignment.topCenter,
+                    icon: Image.asset(
+                      "assets/images/heart.png",
+                      width: 30,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xff90caf9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CommingSoonScreen()),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Blood Sugar",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xffcaf0f8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              width: 350,
+              height: 120,
+            ), // Container1
+
+            SizedBox(
+              height: 20,
+            ), // Sizedbox1
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xff90caf9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CommingSoonScreen()),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Hemoglobin",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xffcaf0f8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              width: 350,
+              height: 120,
+            ), // Container2
+
+            SizedBox(
+              height: 20,
+            ), // Sizedbox2
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xff90caf9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CommingSoonScreen()),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Platelets",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xffcaf0f8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              width: 350,
+              height: 120,
+            ), // Container3
+
+            SizedBox(
+              height: 20,
+            ), // Sizedbox3
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xff90caf9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CommingSoonScreen()),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Blood Pressure",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xffcaf0f8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              width: 350,
+              height: 120,
+            ), // Container4
+
+            SizedBox(
+              height: 20,
+            ), // Sizedbox4
+
+            Container(
+              decoration: BoxDecoration(
+                  color: Color(0xff90caf9),
+                  borderRadius: BorderRadius.circular(10)),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => CommingSoonScreen()),
+                  );
+                },
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Thyroid",
+                      style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            color: Color(0xffcaf0f8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ),
+                  ],
+                ),
+              ),
+              width: 350,
+              height: 120,
+            ), // Container5
+
+            SizedBox(
+              height: 20,
+            ), // Sizedbox5
+          ],
+        ),
+      ),
+
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xff42a5f5),
+              ),
+              child: Text('Hello, Friend!'),
+            ),
+            ListTile(
+              title: const Text('My Dashboard'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('My Profile'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('About App'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
--- gruhini/lib/main.dart
+++ gruhini/lib/main.dart
@@ -42,13 +42,15 @@ class SecondScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xfff8f9fa),
       appBar: AppBar(
-        title: Text("Gruhini App"),
+        title: Text("HoMed"),
         backgroundColor: Color(0xff42a5f5),
       ),
+
       body: Column(
         children: <Widget>[
+
           SizedBox(
-            height: 20,
+            height: 40,
           ),
 
           Padding(
@@ -59,19 +61,23 @@ class SecondScreen extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text("WELCOME TO GRUHINI APP",
+
+                    Text("WELCOME TO HoMed APP",
                         style: TextStyle(
                             color: Color(0xff023e8a),
                             fontSize: 20,
                             fontWeight: FontWeight.bold)),
+
                     SizedBox(
-                      height: 5,
+                      height: 8,
                     ),
+
                     Text("Dashboard",
                         style: TextStyle(
                             color: Color(0xff023e8a),
-                            fontSize: 16,
+                            fontSize: 18,
                             fontWeight: FontWeight.bold)),
+
                   ],
                 ),
                 IconButton(
@@ -87,7 +93,7 @@ class SecondScreen extends StatelessWidget {
           ),
 
           SizedBox(
-            height: 20,
+            height: 40,
           ),
 
           Container(
@@ -108,28 +114,34 @@ class SecondScreen extends StatelessWidget {
                   SizedBox(
                     height: 2,
                   ),
+
                   Image.asset(
                     "assets/images/health.png",
-                    width: 80,
-                    height: 70,
+                    width: 90,
+                    height: 90,
                   ),
+
                   SizedBox(
                     height: 4,
                   ),
+
                   Text(
                     "Health",
                     style: GoogleFonts.openSans(
                         textStyle: TextStyle(
                       color: Color(0xff023e8a),
-                      fontSize: 16,
+                      fontSize: 18,
                       fontWeight: FontWeight.bold,
                     )),
                   ),
+
                   SizedBox(
                     height: 5,
                   ),
+
                   Text(
-                    "Health",
+                    "Health Vitals Status Reporting, Timely Medication Reminder and Vaccination Service",
+                    textAlign: TextAlign.center,
                     style: GoogleFonts.openSans(
                         textStyle: TextStyle(
                       color: Color(0xff023e8a),
@@ -144,7 +156,7 @@ class SecondScreen extends StatelessWidget {
               ),
             ),
             width: 350,
-            height: 180,
+            height: 220,
           ), //container1
 
           SizedBox(
@@ -168,16 +180,19 @@ class SecondScreen extends StatelessWidget {
                   SizedBox(
                     height: 2,
                   ),
+
                   Image.asset(
                     "assets/images/about.png",
-                    width: 80,
-                    height: 70,
+                    width: 90,
+                    height: 90,
                   ),
+
                   SizedBox(
                     height: 4,
                   ),
+
                   Text(
-                    "About app",
+                    "About App",
                     style: GoogleFonts.openSans(
                         textStyle: TextStyle(
                       color: Color(0xff023e8a),
@@ -185,11 +200,14 @@ class SecondScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     )),
                   ),
+
                   SizedBox(
                     height: 5,
                   ),
+
                   Text(
-                    "About app",
+                    "More information about the developers and their purpose",
+                    textAlign: TextAlign.center,
                     style: GoogleFonts.openSans(
                         textStyle: TextStyle(
                       color: Color(0xff023e8a),
@@ -197,6 +215,7 @@ class SecondScreen extends StatelessWidget {
                       fontWeight: FontWeight.bold,
                     )),
                   ),
+
                   SizedBox(
                     height: 2,
                   ),
@@ -204,10 +223,11 @@ class SecondScreen extends StatelessWidget {
               ),
             ),
             width: 350,
-            height: 180,
+            height: 220,
           ), //container3
         ],
       ),
+
       drawer: Drawer(
         // Add a ListView to the drawer. This ensures the user can scroll
         // through the options in the drawer if there isn't enough vertical
